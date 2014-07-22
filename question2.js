1db20369bb75eb541c93d515b2d7fd515e1b60b6
cmathew <chris.w.mathew@gmail.com>
2014-07-21 23:51:17 -0700

    add friends via 'api'
    append to list. Does not check for repeated IDs

--- question2.js
+++ question2.js
@@ -13,6 +13,24 @@ var api = {
 
 $(document).ready(function() {
     // Part 1 YOUR CODE HERE
+	var $friendsList = $('#friends-list').empty();
+	api.listenForFriendChanges(function(data){
+		for (var i in data) {
+			var friend = data[i].friend;
+			switch (data[i].action) {
+				case 'add':
+					var $newLI = $("<li></li>");
+					$newLI.attr('id', 'friend_' + friend.id)
+						.text(friend.name);			
+					$friendsList.append($newLI);
+					break;
+				case 'remove':
+					break;
+				default:
+					break;
+			}	
+		}
+	});
 
     // Part 2 YOUR CODE HERE
 });
