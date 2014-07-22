07102889f9fc275b15eb331b5a99a2b3946ff5e3
cmathew <chris.w.mathew@gmail.com>
2014-07-22 00:09:44 -0700

    clicking friends toggles delete pending state
    still storing data in the DOM, yikes

--- question2.js
+++ question2.js
@@ -13,15 +13,40 @@ var api = {
 
 $(document).ready(function() {
     // Part 1 YOUR CODE HERE
+		
 	var $friendsList = $('#friends-list').empty();
+	var selectedFriends = [];
+	var $deleteFriendButton = $('#delete-friends');
+
+	$friendsList.on('click', '.friend-listing', function(e){
+		var $friend = $(this);		
+		$friend.toggleClass('pending');
+		if ($friend.hasClass('pending')){
+			$friend.css('color', 'red');
+			selectedFriends.push($friend);
+		}
+		else {
+			$friend.css('color', '');
+			selectedFriends.splice(selectedFriends.indexOf($friend), 1);
+		}
+		
+		if (selectedFriends.length) {
+			$deleteFriendButton.show();
+		}
+		else {
+			$deleteFriendButton.hide();		
+		}
+	});
+
 	api.listenForFriendChanges(function(data){
 		for (var i in data) {
 			var friend = data[i].friend;
 			switch (data[i].action) {
 				case 'add':
-					var $newLI = $("<li></li>");
+					var $newLI = $("<li class='friend-listing'></li>");
 					$newLI.attr('id', 'friend_' + friend.id)
-						.text(friend.name);			
+						.text(friend.name)
+						.data('strength', friend.strength);
 					$friendsList.append($newLI);
 					break;
 				case 'remove':
